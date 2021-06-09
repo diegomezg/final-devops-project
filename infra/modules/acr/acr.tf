86587f4c7d199c10cdb2c21d1a0767d7151673e4
Diego Gomez <diego.gomez@digitalonus.com>
2021-06-09 11:27:27 -0500

    ACR name updated

--- infra/modules/acr/acr.tf
+++ infra/modules/acr/acr.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "example" {
-  name                     = "${var.prefix}Acr"
+  name                     = "${var.prefix}-acr"
   resource_group_name      = "${var.resource_group_name}"
   location                 = "${var.location}"
   sku                      = "Standard"
